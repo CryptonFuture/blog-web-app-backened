3294018c9e60bb7a68463a869ddc546132962d95
CryptonFuture <crypton.futuremedia1989@gmail.com>
2025-08-27 15:26:47 +0500

    request count api is created

--- routes/Request/requestRoutes.js
+++ routes/Request/requestRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { addRequest, getRequest, approvedRequest } from '../../controllers/Request/requestController.js'
+import { addRequest, getRequest, countRequest, approvedRequest } from '../../controllers/Request/requestController.js'
 import { auth } from '../../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.post('/addRequest', addRequest)
 router.get('/getRequest', auth, getRequest)
+router.get('/countRequest', auth, countRequest)
 router.put('/approvedRequest/:id', auth, approvedRequest)
 
 export default router
