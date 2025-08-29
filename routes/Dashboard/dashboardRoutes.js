815785e4737877173f56328e5f0577f7be26f496
CryptonFuture <crypton.futuremedia1989@gmail.com>
2025-08-29 18:06:13 +0500

    get role api is created

--- routes/Dashboard/dashboardRoutes.js
+++ routes/Dashboard/dashboardRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { countAll, getSideBarRoutes } from '../../controllers/Dashboard/dashboardController.js'
+import { countAll, getSideBarRoutes, getSideBarRole } from '../../controllers/Dashboard/dashboardController.js'
 import { auth } from '../../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.get('/countAll', auth, countAll)
 router.get('/getSideBarRoutes', auth, getSideBarRoutes)
+router.get('/getSideBarRole', auth, getSideBarRole)
 
 export default router
