cb0ee7cb3a2e926ca5f04da185d9a5f04bdd80fc
CryptonFuture <crypton.futuremedia1989@gmail.com>
2025-09-18 20:21:34 +0500

    config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const env = process.env
 
 
 const port = env.PORT || 8000
-const mongodb_url = env.MONGO_URL || 'mongodb+srv://admim:LelyTJKimfIIFaDw@cluster0.7dfpd9x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongodb_url = env.MONGO_URL 
 const access_token = env.ACCESS_TOKEN_SECRET_KEY || '1234567890'
 const refresh_token = env.REFRESH_TOKEN_SECRET_KEY || 'abcdefghi'
 const secret_key = env.SECRET_KEY || '1234567890'
